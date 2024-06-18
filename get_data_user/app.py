991c273782bc7e2c8b74c9d4b8d9c38c5b62a731
Edson733 <20213tn034@utez.edu.mx>
2024-06-17 19:33:11 -0600

    [DELETE] Files of directory connection_db

--- get_data_user/app.py
+++ get_data_user/app.py
@@ -1,13 +1,9 @@
 import json
-import sys
 import os
 import logging
 
-# Añade el directorio connection_db al sys.path
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'connection_db'))
-
 # Importa las funciones necesarias desde connection_db
-from connection_db.connection_db import connect_db, execute_query, close_connection
+from .connection_db import connect_db, execute_query, close_connection
 
 logging.basicConfig(level=logging.INFO)
 
