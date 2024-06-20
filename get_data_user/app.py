3818aef54c4b12c5ec21af57e7f9fb29dd648a8e
Edson733 <20213tn034@utez.edu.mx>
2024-06-19 21:04:33 -0600

    [FIX] get_data_user and implements unit test

--- get_data_user/app.py
+++ get_data_user/app.py
@@ -3,29 +3,38 @@ import os
 import logging
 
 # Importa las funciones necesarias desde connection_db
-from connection_db import connect_db, execute_query, close_connection
+from .connection_db import connect_db, execute_query, close_connection
 
+# Configura el nivel de logging a INFO
 logging.basicConfig(level=logging.INFO)
 
 
+# Definición de la función lambda_handler, la cual es el punto de entrada para la ejecución Lambda
 def lambda_handler(event, context):
+    # Obtiene las variables de entorno necesarias para la conexión a la base de datos
     rds_host = os.environ['RDS_HOST']
     rds_user = os.environ['DB_USERNAME']
     rds_password = os.environ['DB_PASSWORD']
     rds_db = os.environ['DB_NAME']
 
+    # Define la consulta SQL que se ejecutará
     query = f"SELECT * FROM users;"
 
+    # Establece la conexión a la base de datos usando las credenciales y parámetros obtenidos
     connection = connect_db(rds_host, rds_user, rds_password, rds_db)
 
+    # Inicializa una lista vacía para almacenar los usuarios
     users = []
 
+    # Verifica si la conexión fue exitosa
     if connection:
         try:
+            # Ejecuta la consulta SQL
             results = execute_query(connection, query)
-            close_connection(connection)
+            # Verifica si se obtuvieron resultados
             if results:
                 logging.info("Results:")
+                # Itera sobre cada fila de resultados y los almacena en el diccionario `user`
                 for row in results:
                     user = {
                         'id_usr': row[0],
@@ -34,8 +43,11 @@ def lambda_handler(event, context):
                         'email_usr': row[3],
                         'phone_usr': row[4]
                     }
+                    # Añade el diccionario `user` a la lista `users`
                     users.append(user)
+                    # Loggea cada fila obtenida
                     logging.info(row)
+                # Retorna un diccionario con el código de estado 200 y los datos de los usuarios en formato JSON
                 return {
                     "statusCode": 200,
                     "body": json.dumps({
@@ -44,18 +56,26 @@ def lambda_handler(event, context):
                     })
                 }
             else:
+                # Retorna un diccionario con el código de estado 204 si no se encontraron resultados
                 return {
                     "statusCode": 204,
                     "body": json.dumps({"message": "No results found."})
                 }
         except Exception as e:
+            # Captura y loggea cualquier excepción ocurrida durante la ejecución de la consulta
             logging.error("Error executing query: %s", e)
+            # Retorna un diccionario con el código de estado 500 y un mensaje de error
             return {
                 "statusCode": 500,
                 "body": json.dumps({"error": "An error occurred while processing the request."})
             }
+        finally:
+            # Cierra la conexión a la base de datos
+            close_connection(connection)
     else:
+        # Loggea un error si la conexión a la base de datos falló
         logging.error("Connection to the database failed.")
+        # Retorna un diccionario con el código de estado 500 y un mensaje de error
         return {
             "statusCode": 500,
             "body": json.dumps({"error": "Failed to connect to the database."})
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,72 +1,110 @@
 import json
-
-import pytest
-
+import os
+import unittest
+from unittest.mock import patch
 from get_data_user import app
 
 
-@pytest.fixture()
-def apigw_event():
-    """ Generates API GW Event"""
+# Define una clase para pruebas unitarias de la aplicación
+class TestApp(unittest.TestCase):
 
-    return {
-        "body": '{ "test": "body"}',
-        "resource": "/{proxy+}",
-        "requestContext": {
-            "resourceId": "123456",
-            "apiId": "1234567890",
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
-            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
-            "accountId": "123456789012",
-            "identity": {
-                "apiKey": "",
-                "userArn": "",
-                "cognitoAuthenticationType": "",
-                "caller": "",
-                "userAgent": "Custom User Agent String",
-                "user": "",
-                "cognitoIdentityPoolId": "",
-                "cognitoIdentityId": "",
-                "cognitoAuthenticationProvider": "",
-                "sourceIp": "127.0.0.1",
-                "accountId": "",
-            },
-            "stage": "prod",
-        },
-        "queryStringParameters": {"foo": "bar"},
-        "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-            "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-            "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "Cache-Control": "max-age=0",
-            "User-Agent": "Custom User Agent String",
-            "CloudFront-Forwarded-Proto": "https",
-            "Accept-Encoding": "gzip, deflate, sdch",
-        },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
-        "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
-    }
+    # Prueba para una ejecución exitosa del manejador lambda con resultados
+    @patch.dict(os.environ, {
+        'RDS_HOST': 'test_host',  # Imita la variable de entorno para el host de la BD
+        'DB_USERNAME': 'test_user',  # Imita la variable de entorno para el usuario de la BD
+        'DB_PASSWORD': 'test_password',  # Imita la variable de entorno para la contraseña de la BD
+        'DB_NAME': 'test_db'  # Imita la variable de entorno para el nombre de la BD
+    })
+    @patch('get_data_user.app.connect_db')  # Imita la función connect_db
+    @patch('get_data_user.app.execute_query')  # Imita la función execute_query
+    @patch('get_data_user.app.close_connection')  # Imita la función close_connection
+    def test_lambda_handler_success(self, mock_close_connection, mock_execute_query, mock_connect_db):
+        # Simula una conexión exitosa y resultados de consulta
+        mock_connect_db.return_value = True  # Imita connect_db para devolver True indicando una conexión exitosa
+        mock_execute_query.return_value = [  # Imita execute_query para devolver datos de usuario de ejemplo
+            {
+                "id_usr": 1,
+                "name_usr": 'John',
+                "lastname_usr": 'Doe',
+                "email_usr": 'john.doe@example.com',
+                "phone_usr": '1234567890'
+            }
+        ]
+        event = {}  # Define un evento simulado para el manejador lambda
+        context = None  # Define un contexto simulado para el manejador lambda
+        result = app.lambda_handler(event, context)  # Llama al manejador lambda
+        self.assertEqual(result['statusCode'], 200)  # Verifica que el código de estado sea 200 (OK)
+        body = json.loads(result['body'])  # Analiza el cuerpo de la respuesta
+        self.assertEqual(body['message'], 'Get Users')  # Verifica que el mensaje en la respuesta sea 'Get Users'
+        self.assertTrue('data' in body)  # Verifica que la respuesta contenga 'data'
+        self.assertEqual(len(body['data']), 1)  # Verifica que 'data' contenga exactamente un usuario
+        mock_close_connection.assert_called_once_with(True)  # Verifica que close_connection sea llamado con True
 
+    # Prueba para una ejecución exitosa del manejador lambda sin resultados
+    @patch.dict(os.environ, {
+        'RDS_HOST': 'test_host',
+        'DB_USERNAME': 'test_user',
+        'DB_PASSWORD': 'test_password',
+        'DB_NAME': 'test_db'
+    })
+    @patch('get_data_user.connect_db')
+    @patch('get_data_user.execute_query')
+    @patch('get_data_user.close_connection')
+    def test_lambda_handler_no_results(self, mock_close_connection, mock_execute_query, mock_connect_db):
+        # Simula una conexión exitosa pero sin resultados de consulta
+        mock_connect_db.return_value = True
+        mock_execute_query.return_value = []  # Imita execute_query para devolver una lista vacía
+        event = {}
+        context = None
+        result = app.lambda_handler(event, context)
+        self.assertEqual(result['statusCode'], 204)  # Verifica que el código de estado sea 204 (Sin Contenido)
+        body = json.loads(result['body'])
+        # Verifica el mensaje en la respuesta sea 'No results found.'
+        self.assertEqual(body['message'], 'No results found.')
+        mock_close_connection.assert_called_once_with(True)
 
-def test_lambda_handler(apigw_event):
+    # Prueba para una ejecución del manejador lambda con un error en la consulta
+    @patch.dict(os.environ, {
+        'RDS_HOST': 'test_host',
+        'DB_USERNAME': 'test_user',
+        'DB_PASSWORD': 'test_password',
+        'DB_NAME': 'test_db'
+    })
+    @patch('get_data_user.connect_db')
+    @patch('get_data_user.execute_query')
+    @patch('get_data_user.close_connection')
+    def test_lambda_handler_query_error(self, mock_close_connection, mock_execute_query, mock_connect_db):
+        # Simula una conexión exitosa pero un error en la ejecución de la consulta
+        mock_connect_db.return_value = True
+        # Imita execute_query para lanzar una excepción
+        mock_execute_query.side_effect = Exception('Query execution error')
+        event = {}
+        context = None
+        result = app.lambda_handler(event, context)
+        # Verifica que el código de estado sea 500 (Error Interno del Servidor)
+        self.assertEqual(result['statusCode'], 500)
+        body = json.loads(result['body'])
+        # Verifica que el error en la respuesta sea 'An error occurred while processing the request.'
+        self.assertEqual(body['error'], 'An error occurred while processing the request.')
+        mock_close_connection.assert_called_once_with(True)
 
-    ret = app.lambda_handler(apigw_event, "")
-    data = json.loads(ret["body"])
-
-    assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
+    # Prueba para una ejecución del manejador lambda con un fallo en la conexión a la BD
+    @patch.dict(os.environ, {
+        'RDS_HOST': 'test_host',
+        'DB_USERNAME': 'test_user',
+        'DB_PASSWORD': 'test_password',
+        'DB_NAME': 'test_db'
+    })
+    @patch('get_data_user.connect_db')
+    def test_lambda_handler_connection_fail(self, mock_connect_db):
+        # Simula un fallo en la conexión a la base de datos
+        mock_connect_db.return_value = None  # Imita connect_db para devolver None indicando un fallo en la conexión
+        event = {}
+        context = None
+        result = app.lambda_handler(event, context)
+        self.assertEqual(result['statusCode'], 500)
+        body = json.loads(result['body'])
+        # Verifica que el error en la respuesta sea 'Failed to connect to the database.'
+        self.assertEqual(body['error'], 'Failed to connect to the database.')
+        # Verifica que connect_db se haya llamado una vez con los parámetros correctos
+        mock_connect_db.assert_called_once_with('test_host', 'test_user', 'test_password', 'test_db')
